d0fc756c2bcbe7f10fa9566b348c87ed95fe305f
shyamkishores001 <107265443+shyamkishores001@users.noreply.github.com>
2022-11-10 18:26:48 +0000

    Add files via upload

---
+++ main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.*;
+import com.example.demo.entity.*;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@PostMapping("/add")
+	public User add(@RequestBody User user) {
+		
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/all")
+	public List<User> getUser(){
+		return userRepository.findAll();
+	}
+	
+
+}
---
+++ main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,79 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.repository.*;
+import com.example.demo.entity.*;
+
+@Service
+public class EmployeeService {
+	
+	
+	
+	EmployeeRepository employeeRepository;
+	
+	
+	public List<Employee> getAll(){
+		return employeeRepository.findAll();
+	}
+	
+
+	
+	@Transactional
+	
+	public Employee save(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	@Transactional
+	
+	public Employee findById(int id) {
+		
+		Optional<Employee> employee = employeeRepository.findById(id);
+		Employee employee2 = employee.get();
+		return employee2;
+	}
+	
+	@Transactional
+
+	public String deleteById(int id) {
+		Employee employee = employeeRepository.findById(id).get();
+		employeeRepository.delete(employee);
+		return "Deleted Employee id "+id;
+	}
+	
+	@Transactional
+	
+	public Employee update(Employee emp) {
+		return employeeRepository.save(emp);
+	}
+	
+	
+	public List<Employee> findByName(String name) {
+		return employeeRepository.findByFirstname(name);
+	}
+	
+
+	@Transactional
+	public List<Employee> sort(String order){
+		
+	
+		if(order.equals("\"asc\"")) {
+			System.out.println(order);
+			return employeeRepository.findAll(Sort.by(Direction.ASC, "firstname"));
+		}
+		System.out.println(order);
+		return employeeRepository.findAll(Sort.by(Direction.DESC, "firstname"));
+	}
+
+
+}
